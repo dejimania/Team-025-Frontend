a0bf04431f7dfe39cfeb68c10f304e6de0b05cb9
busry <minitext@gmail.com>
2020-08-22 13:58:02 +0100

    add build folder

---
+++ build/precache-manifest.0df000685184c4eb03335c4b55304f20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea0986c84263ec8413185d2696fdab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06e1811ac73fa711caf4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2f72091bf620ceb36df",
+    "url": "/static/js/2.19be690c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19be690c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e1811ac73fa711caf4",
+    "url": "/static/js/main.45098e42.chunk.js"
+  },
+  {
+    "revision": "325079437b200fc3fda5",
+    "url": "/static/js/runtime-main.274ff8c7.js"
+  }
+]);
