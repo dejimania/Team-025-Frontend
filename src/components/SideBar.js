5ce15e1a3cd504b6298e4bb120dfdafa8bde6e39
Oladehinde Olanrewaju Kazeem <larrysnet2001@gmail.com>
2020-10-04 12:49:13 +0100

    feat(favicon sidebar): fix favcion and sidebar
    - add favicon
    - change manifest
    - add active class to sidebar
    
    [Fixed #20]

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,7 +9,7 @@ import { GiWaterDrop } from 'react-icons/gi';
 import {  BiPowerOff } from 'react-icons/bi';
 // import { GrConnectivity } from 'react-icons/gr';
 import './sidebar.css'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { SET_SIDEBAR } from '../store/types/sideBarTypes';
 
 const SideBar = () => {
@@ -36,15 +36,15 @@ const SideBar = () => {
       </div>
       <Nav  className="navs">
 
-        <Nav.Link as={Link} onClick={()=> dispatch({type: SET_SIDEBAR})} exact="true" to="/dashboard" className=""><MdDashboard className="mr-4" size="1.5rem"/>Dashboard</Nav.Link>
-        <Nav.Link as={Link} onClick={()=> dispatch({type: SET_SIDEBAR})} exact="true" to="/donation" eventKey="link-1"><GiWaterDrop className="mr-4" size="1.5rem"/>Donate</Nav.Link>
-        <Nav.Link as={Link} onClick={()=> dispatch({type: SET_SIDEBAR})} exact="true" to="/requests" eventKey="link-1"><VscRequestChanges className="mr-4" size="1.5rem"/>Requests</Nav.Link>
-        <Nav.Link as={Link} onClick={()=> dispatch({type: SET_SIDEBAR})} exact="true" to="/donation/history" eventKey="link-2"><VscCalendar className="mr-4" size="1.5rem"/>Donation History</Nav.Link>
+        <Nav.Link as={NavLink}  activeClassName="active" onClick={()=> dispatch({type: SET_SIDEBAR})} to="/dashboard" className=""><MdDashboard className="mr-4" size="1.5rem"/>Dashboard</Nav.Link>
+        <Nav.Link as={NavLink} onClick={()=> dispatch({type: SET_SIDEBAR})} to="/donation" eventKey="link-1"><GiWaterDrop className="mr-4" size="1.5rem"/>Donate</Nav.Link>
+        <Nav.Link as={NavLink} onClick={()=> dispatch({type: SET_SIDEBAR})} to="/requests" eventKey="link-1"><VscRequestChanges className="mr-4" size="1.5rem"/>Requests</Nav.Link>
+        <Nav.Link as={NavLink} onClick={()=> dispatch({type: SET_SIDEBAR})} to="/donation/history" eventKey="link-2"><VscCalendar className="mr-4" size="1.5rem"/>Donation History</Nav.Link>
 
         <div className="bg-danger w-75 mb-3 mt-1 mx-auto" style={{height: '0.1rem', opacity: '0.3'}}></div>
 
         <Nav.Link eventKey="link-2"><RiBankCardLine className="mr-4" size="1.5rem"/>Banks</Nav.Link>
-        <Nav.Link eventKey="link-2"  as={Link} to="/profile"><RiBankCardLine className="mr-4" size="1.5rem"/>Profile</Nav.Link>
+        <Nav.Link eventKey="link-2"  as={NavLink} to="/profile"><RiBankCardLine className="mr-4" size="1.5rem"/>Profile</Nav.Link>
         <Nav.Link eventKey="link-2"><GiWaterDrop className="mr-4" size="1.5rem"/>Get Help</Nav.Link>
 
         <div className="bg-danger w-75 mb-3 mt-1 mx-auto" style={{height: '0.1rem', opacity: '0.3'}}></div>
